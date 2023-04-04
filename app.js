e8144c079739dd7a65449589523da7ec463d151d
Marina Malyarchuk <pchupchu@yandex.ru>
2023-04-04 14:48:52 +0300

    fix connection to db

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/mestodb");
+mongoose
+  .connect("mongodb://127.0.0.1/mestodb")
+  .then(() => console.log("DB is connected"))
+  .catch((err) => console.log(err));
 
 app.use("/users", require("./routes/users"));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
+
+//"_id": "642c0bf809c98eefc507db63",
