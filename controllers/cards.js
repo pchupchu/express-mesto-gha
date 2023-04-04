a7de2acc074c3b964f78ed89ca45a3c6ced2ad7b
Marina Malyarchuk <pchupchu@yandex.ru>
2023-04-04 22:27:55 +0300

    add populate for cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,12 +5,14 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner })
+    .populate("owner")
     .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.getCards = (req, res) => {
   Card.find({})
+    .populate("owner")
     .then((cards) => res.send({ data: cards }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
@@ -27,6 +29,7 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true }
   )
+    .populate("owner")
     .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
@@ -37,6 +40,7 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true }
   )
+    .populate("owner")
     .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
