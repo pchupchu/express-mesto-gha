8ff3f0bc94597e5940214ea0e41fe79846da6fd7
Marina Malyarchuk <pchupchu@yandex.ru>
2023-04-19 20:22:42 +0300

    add hash password

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,19 @@
 const User = require("../models/user");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const ERROR_BAD_REQUEST = 400;
 const ERROR_NOT_FOUND = 404;
 const OTHER_ERROR = 500;
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
-    .then((user) => res.status(201).send({ data: user }))
+  const { name, about, avatar, email, password } = req.body;
+  bcrypt
+    .hash(password, 6)
+    .then((hash) => {
+      User.create({ name, about, avatar, email, password: hash }).then((user) =>
+        res.status(201).send({ data: user })
+      );
+    })
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError)
         return res.status(ERROR_BAD_REQUEST).send({
--- models/user.js
+++ models/user.js
@@ -6,16 +6,31 @@ const userSchema = new mongoose.Schema({
     required: true,
     minLength: 2,
     maxLength: 30,
+    default: "Жак-Ив Кусто",
   },
   about: {
     type: String,
     required: true,
     minLength: 2,
     maxLength: 30,
+    default: "Исследователь",
   },
   avatar: {
     type: String,
     required: true,
+    default:
+      "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    select: false,
   },
 });
 
