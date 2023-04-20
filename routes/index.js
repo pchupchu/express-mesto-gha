bb7d7f407265265506c46822841980ded88f5599
Marina Malyarchuk <pchupchu@yandex.ru>
2023-04-20 17:13:26 +0300

    add auth

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const { createUser, login } = require("../controllers/users");
+const auth = require("../middlewares/auth");
 
 const userRouter = require("./users");
 const cardRouter = require("./cards");
 
-router.use("/users", userRouter);
-router.use("/cards", cardRouter);
+router.use("/users", auth, userRouter);
+router.use("/cards", auth, cardRouter);
 router.post("/signin", login);
 router.post("/signup", createUser);
 
