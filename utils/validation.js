70c920a3c47b2e3374f5c83a8cdc2df41fc11392
Marina Malyarchuk <pchupchu@yandex.ru>
2023-04-23 00:49:16 +0300

    fix validation

--- utils/validation.js
+++ utils/validation.js
@@ -34,18 +34,18 @@ module.exports.validationUpdateUser = celebrate({
 
 module.exports.validationUpdateAvatar = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().pattern(
-      new RegExp('^(https?:\\/\\/)(www\\.)?[^\\s$.?#].[^\\s]*$')
-    ),
+    avatar: Joi.string()
+      .required()
+      .pattern(new RegExp('^(https?:\\/\\/)(www\\.)?[^\\s$.?#].[^\\s]*$')),
   }),
 });
 
 module.exports.validationCreateCard = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    link: Joi.string().pattern(
-      new RegExp('^(https?:\\/\\/)(www\\.)?[^\\s$.?#].[^\\s]*$')
-    ),
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string()
+      .required()
+      .pattern(new RegExp('^(https?:\\/\\/)(www\\.)?[^\\s$.?#].[^\\s]*$')),
   }),
 });
 
