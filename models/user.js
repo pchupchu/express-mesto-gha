a1fa668d2bed6f1c2d4664eeb4a6bebbbee6345e
Marina Malyarchuk <pchupchu@yandex.ru>
2023-04-24 10:14:08 +0300

    new try run test

--- models/user.js
+++ models/user.js
@@ -20,14 +20,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     default:
       'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    // validate: {
-    //   validator: () =>
-    //     validator.isURL({
-    //       protocols: ['http', 'https'],
-    //       require_protocol: true,
-    //     }),
-    //   message: 'Неправильный формат ссылки',
-    // },
+    validate: {
+      validator: (v) =>
+        validator.isURL(v, {
+          protocols: ['http', 'https'],
+          require_protocol: true,
+        }),
+      message: 'Неправильный формат ссылки',
+    },
   },
   email: {
     type: String,
